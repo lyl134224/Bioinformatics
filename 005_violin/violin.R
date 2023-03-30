a038850beb06f40744b0530010ec8a43c1639eb4
lyl134224 <77323849+lyl134224@users.noreply.github.com>
2023-03-30 17:10:36 +0800

    Add files via upload

--- 005_violin/violin.R
+++ 005_violin/violin.R
@@ -1,1 +1,12 @@
+##violin 
+##2022-09-06
+##by yanlin liao
 
+
+library(ggplot2)
+
+
+p <- ggplot(data, aes(x, y, fill=group)) +  #按组填充颜色 
+     geom_violin() + 
+     mytheme + 
+     xlab(x) + ylab(y)
